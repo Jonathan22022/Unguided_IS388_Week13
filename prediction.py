8d1670b64b622044505a7241721777edaa611ddf
Jonathan22022 <129663657+Jonathan22022@users.noreply.github.com>
2025-12-04 11:33:29 +0700

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,5 +1,11 @@
+# prediction.py
 import joblib
 
 def predict(data):
-    clf = joblib.load("dbscan_model.sav")
-    return clf.predict(data)
+    model = joblib.load("dbscan_model.sav")
+    scaler = joblib.load("scaler_dbscan.sav")
+
+    data_scaled = scaler.transform(data)
+    prediction = model.fit_predict(data_scaled)
+
+    return prediction
